1f2ebcba5fd507db380ede21e1cf749bb4808cae
Vinyurchin <91989971+Vinyurchin@users.noreply.github.com>
2021-11-19 23:41:47 -0600

    Add files via upload

---
+++ Funcion_algebraica_3.m
@@ -0,0 +1,36 @@
+% Octava.funcion3
+% Alumno: Diego Cruz Garduño
+% Escuela: Tecnologico de estudios superiores de Jilotepec
+% Carrera: Ingenieria en sistemas computacionales
+% Matricula: 202123339
+% Actividad: Funciones algebraicas- polinomiales y racionales
+% Versión 1
+% Descripcion: Requiere el paquete simbólico
+
+clear
+%Intervalo
+x = -100: 0.1:100;
+%Funcion 
+fx = x.^(2./3);
+% Plotear
+plot(x, fx);
+grid
+% Titulo 
+title(['\fontsize{20} funcion f(x)=^{3}\surdx^{2}']);
+% Etiqueta para eje de las abscisas (x)
+xlabel(['Dominio en X']);
+% Etiqueta para eje de las ordenadas (y)
+ylabel(['Rango en Y']);
+
+text(-0,-10,strcat('\fontsize{20}Funcion exponencial'));
+
+% IMPRESIONES
+f = 'f(x)=x^2/3';
+e = 'EXPONENCIAL, NO ES POLINOMICA';
+e2 = 'f(0)=0  (0,0)   x=0';
+disp('');
+disp(['La funcion es: ', num2str(f)]);
+disp('');
+disp(['Esta es una funcion: ', num2str(e)]);
+disp(['El punto donde f(x) es igual a 0 es: ', num2str(e2)]);
+disp('');
